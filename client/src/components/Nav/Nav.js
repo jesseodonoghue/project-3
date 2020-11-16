6dfaf1aa82746a102bea048a60b34c8c6e5ec869
Nalee Star <ahn.jinhi@gmail.com>
2020-11-15 19:11:47 -0500

    linked pages to nav

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -1,8 +1,12 @@
 import React, { Fragment } from "react";
-import { Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 // import { Col } from '../Grid';
 import './Nav.css';
 import { Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import Connections from '../../pages/Matching'; 
+import Posts from '../../pages/Posts'; 
+import Dashboard from '../../pages/ProfileDash'; 
+import Profile from '../../pages/ProfileSetGen';
 
 // we can import the bootstrap nav module here, then delete Link and Col from above
 
@@ -12,24 +16,31 @@ const navBar = (props) => {
   return (
 
     // we would put the nav module here and delete this stuff, then put in our own Link pages (Connections, Posts, Dashboard, Settings(this one has sub links))
+    <Router>
 
-    <Navbar className="color-nav" collapseOnSelect expand="lg" variant="dark">
-      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-      <Navbar.Collapse id="responsive-navbar-nav">
-        <Nav className="ml-auto">
-        <Link className="nav-link" to="./connections" >Connections</Link> 
-          <Link className="nav-link" to="./posts" >Posts</Link>
-          <Link className="nav-link" to="./dashboard" >Dashboard</Link>
-          <NavDropdown title="Settings" id="collasible-nav-dropdown">
-            <NavDropdown.Item href="./profile">Profile</NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item to="#" className="logout" onClick={props.logout}>Logout</NavDropdown.Item>
-          </NavDropdown>
-          <Navbar.Brand href="./">DBUGME</Navbar.Brand>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
+      <Navbar className="color-nav" collapseOnSelect expand="lg" variant="dark">
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="ml-auto">
+            <Link className="nav-link" to="/matching" >Connections</Link>
+            <Link className="nav-link" to="/posts" >Posts</Link>
+            <Link className="nav-link" to="/dashboard" >Dashboard</Link>
+            <NavDropdown title="Settings" id="collasible-nav-dropdown">
+              <NavDropdown.Item href="/profilesetgen">Profile</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item to="#" className="logout" onClick={props.logout}>Logout</NavDropdown.Item>
+            </NavDropdown>
+            <Navbar.Brand href="/">DBUGME</Navbar.Brand>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
 
+    <Route path="/matching" exact component={Connections} />
+    <Route path="/posts" exact component={Posts} />
+    <Route path="/dashboard" exact component={Dashboard} />
+    <Route path="/profilesetgen" exact component={Profile} />
+
+    </Router>
   );
 };
 
