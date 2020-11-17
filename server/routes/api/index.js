a89fd5c3c949142df10d29bfe4249f6de4d88e0f
Victoria Greenfield <victoriagfield@gmail.com>
2020-11-16 19:43:55 -0500

    updated index in api

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -1,7 +1,9 @@
 const router = require("express").Router();
 const postsRoutes = require("./posts");
+const userRoutes = require("./users");
 
 // Book routes
 router.use("/posts", postsRoutes);
+router.use("/users", userRoutes)
 
 module.exports = router;
