bace68c6d0053d4ac4f9d70f0a3c8d9c3fdeac5a
Nalee Star <ahn.jinhi@gmail.com>
2020-11-20 13:23:03 -0500

    updated profilesetnet

--- client/src/pages/ProfileSetNet/ProfileSetNet.js
+++ client/src/pages/ProfileSetNet/ProfileSetNet.js
@@ -1,11 +1,91 @@
 import React from 'react';
+import Sidebar from '../../components/Sidebar';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { Container, Row, Col, } from 'react-bootstrap';
+import { Input, FormBtn } from '../../components/Form';
+import ProfileSetGen from '../../pages/ProfileSetGen';
+import './ProfileSetNet.css';
+import { MDBContainer, MDBRow, MDBCol, MDBInput, MDBFormInline, MDBForm } from "mdbreact";
+
 
 export default function ProfileSetNet() {
     return (
-    
-        <div>
-            <h1>ProfileSetNet</h1>
-            
-        </div>
+
+        <>
+            <div className="container-flex">
+                <Sidebar width={450} height={'170vh'}>
+                    <Container>
+                        <Col size="md-3">
+                            <h4 style={{ color: 'white' }}>SETTINGS</h4>
+                        </Col>
+                        <Col className="text-right" size="md-4" style={{ marginTop: "3em", color: 'white' }}>
+                            {/* using short syntax for React fragment with <></> */}
+                            <> <h5>GENERAL</h5>
+                                <hr />
+                                <Link className='nav-link' to='/profilesetgen'>
+                                    <h6 style={{ color: 'white' }}>Personal Info</h6>
+                                </Link>
+                            </>
+                            <><h5 style={{ marginTop: "1em" }}>NETWORKING</h5>
+                                <hr />
+                                <Link className='nav-link' to='/profilesetnet'>
+                                    <h6 style={{ color: 'white' }}>Skills Page</h6>
+                                </Link>
+                            </>
+                        </Col>
+                    </Container>
+                </Sidebar>
+                <div className="updateSkills">
+                    <h1 style={{ color: '#8860D0' }}>Update Your Skills</h1>
+
+                    <MDBContainer className="checklistGrid">
+                        <MDBRow>
+                            <MDBCol md="4">Skill</MDBCol>
+                            <MDBCol md="4">Mentor</MDBCol>
+                            <MDBCol md="4">Student</MDBCol>
+                        </MDBRow>
+                        <MDBRow>
+                            <MDBCol className="skill-title" md="4">Whatever Skill</MDBCol>
+                            <MDBCol>
+                            <MDBInput  type="checkbox" id="checkbox1" />
+                            </MDBCol>
+                            <MDBCol>
+                            <MDBInput  type="checkbox" id="checkbox2" />
+                            </MDBCol>
+                        </MDBRow>
+                        <MDBRow>
+                            <MDBCol className="skill-title" md="4">Whatever Skill</MDBCol>
+                            <MDBCol>
+                            <MDBInput  type="checkbox" id="checkbox1" />
+                            </MDBCol>
+                            <MDBCol>
+                            <MDBInput  type="checkbox" id="checkbox2" />
+                            </MDBCol>
+                        </MDBRow>
+                        <MDBRow>
+                            <MDBCol className="skill-title" md="4">Whatever Skill</MDBCol>
+                            <MDBCol>
+                            <MDBInput  type="checkbox" id="checkbox1" />
+                            </MDBCol>
+                            <MDBCol>
+                            <MDBInput  type="checkbox" id="checkbox2" />
+                            </MDBCol>
+                        </MDBRow>
+                        <MDBRow>
+                            <MDBCol className="skill-title" md="4">Whatever Skill</MDBCol>
+                            <MDBCol>
+                            <MDBInput  type="checkbox" id="checkbox1" />
+                            </MDBCol>
+                            <MDBCol>
+                            <MDBInput  type="checkbox" id="checkbox2" />
+                            </MDBCol>
+                        </MDBRow>
+                    </MDBContainer>
+                    <FormBtn style={{ color: '#5680e9' }}>Save Changes</FormBtn>
+                </div>
+            </div>
+
+        </>
     )
 }
+
