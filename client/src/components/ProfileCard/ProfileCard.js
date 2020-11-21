63a209d7f4bf3b1691bd77126ea64fbb16bf5dc1
Chelsea Haviland <chelsea.haviland.13@gmail.com>
2020-11-21 12:48:02 -0500

    added default profile image

--- client/src/components/ProfileCard/ProfileCard.js
+++ client/src/components/ProfileCard/ProfileCard.js
@@ -3,142 +3,10 @@ import { Redirect, Link } from 'react-router-dom';
 import { Container, Row, Col } from '../../components/Grid';
 import { Card } from '../../components/Card';
 import { Input, FormBtn } from '../../components/Form';
-<<<<<<< HEAD
-import Button from 'react-bootstrap/Button';
-// import AUTH from '../../utils/AUTH';
-import ProfilePic from '../../assets/defaultprofilepic.svg'
-import Image from 'react-bootstrap/Image'
-import './style.css'
-
-
-
-function ProfileCard() {
-  const [userObject, setUserObject] = useState({
-    firstName: '',
-    lastName: '',
-    email: '',
-    github: '',
-    linkedin: '',
-    // redirectTo: null
-  });
-//   const [redirectTo, setRedirectTo] = useState(null);
-
-//   const handleChange = (event) => {
-// 		setUserObject({
-//       ...userObject,
-// 			[event.target.name]: event.target.value
-// 		});
-// 	};
-  
-// 	const handleSubmit = (event) => {
-// 		event.preventDefault();
-// 		// TODO - validate!
-// 		AUTH.signup({
-//       firstName: userObject.firstName,
-//       lastName: userObject.lastName,
-//       email: userObject.email,
-//       password: userObject.password
-//     }).then(response => {
-//       // console.log(response);
-//       if (!response.data.errmsg) {
-//         setRedirectTo('/');
-//       } else {
-//         console.log('duplicate');
-//       }
-//     });
-//   };
-  
-//   if (redirectTo) {
-//     return <Redirect to={{ pathname: redirectTo }} />
-//   }
-  
-  return (
-    <div style={{ display: "flex", flexDirection: "column", height: "100%"}}>
-        {/* //// Profile Photo Card //// */}
-        <h1 style={{ color: '#5680e9' }}>Profile Photo</h1>
-        <Card className="profile-photo" style={{ background: '#5680e9'}}>
-        </Card> 
-            <div style={{ marginTop: "3em"}}>
-                <h1 style={{ color: '#5680e9' }}>Personal</h1>
-                <Card title="Profile settings">
-                    <form style={{marginTop: 10}}>
-                            <label htmlFor="firstName">First name: </label>
-                        <Input
-                            type="text"
-                            name="firstName"
-                            // value={userObject.firstName}
-                            // onChange={handleChange}
-                        />
-                            <label htmlFor="lastName">Last name: </label>
-                        <Input
-                            type="text"
-                            name="lastName"
-                            // value={userObject.lastName}
-                            // onChange={handleChange}
-                        />
-                            <label htmlFor="email">Email: </label>
-                        <Input
-                            type="text"
-                            name="email"
-                            // value={userObject.github}
-                            // onChange={handleChange}
-                        />
-                            <label htmlFor="password">GitHub: </label>
-                        <Input
-                            type="text"
-                            name="github"
-                            // value={userObject.linkedin}
-                            // onChange={handleChange}
-                        />
-                            <label htmlFor="confirmPassword">LinkedIn: </label>
-                        <Input
-                            type="password"
-                            name="confirmPassword"
-                            // value={userObject.confirmPassword}
-                            // onChange={handleChange}
-                        />
-                        <FormBtn >Change Info</FormBtn>
-                    </form>
-                </Card>
-            </div>
-            
-        {/* //// Password Change Form //// */}
-
-        <div style={{ marginTop: "3em"}}>
-                <h1 style={{ color: '#5680e9' }}>Change Password</h1>
-            <Card title="Create an Account">
-                <form style={{marginTop: 10}}>
-                    <label htmlFor="firstName">Current Password </label>
-                    <Input
-                        type="text"
-                        name="firstName"
-                        // value={userObject.firstName}
-                        // onChange={handleChange}
-                    />
-                    <label htmlFor="lastName">New Password </label>
-                    <Input
-                        type="text"
-                        name="lastName"
-                        // value={userObject.lastName}
-                        // onChange={handleChange}
-                    />
-                    <label htmlFor="email">Confirm Password </label>
-                    <Input
-                        type="text"
-                        name="email"
-                        // value={userObject.email}
-                        // onChange={handleChange}
-                    />
-                    <FormBtn style={{ color: '#5680e9' }}>Change Password</FormBtn>
-                </form>
-            </Card>
-        </div>
-    </div>
-  )
-=======
 import AUTH from '../../utils/AUTH';
 import API from "../../utils/API";
-import bgImg from '../../assets/profilebg.svg';
+import ProfilePic from '../../assets/defaultprofilepic.svg'
+import Image from 'react-bootstrap/Image'
 import './style.css'
 
 
@@ -236,8 +104,10 @@ function ProfileCard() {
             {!loading && (
                 <div style={{ display: "flex", flexDirection: "column", height: "100%"}}>
                     {/* //// Profile Photo Card //// */}
-                    <h3 style={{ color: '#5680e9' }}>Profile Photo</h3>
+                    <h3 style={{ color: '#5680E9' }}>Profile Photo</h3>
                     <Card className="profile-photo" style={{ background: '#5680e9'}}>
+                        <Image src={ProfilePic} alt="ProfilePic" fluid/>
+                        <FormBtn style={{ color: "#5680E9", marginTop: ".5em" }}>Edit/Update Profile Picture</FormBtn>
                     </Card> 
                         <div style={{ marginTop: "3em"}}>
                             <h3 style={{ color: '#5680e9' }}>Personal</h3>
@@ -316,7 +186,6 @@ function ProfileCard() {
 
         </>
     )
->>>>>>> 4cf33d64b189fbb6f396b9b885b035d36bc85d61
 }
 
 export default ProfileCard;
