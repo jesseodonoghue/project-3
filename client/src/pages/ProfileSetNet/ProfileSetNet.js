9dc66515461e2a6c490e5dc62567b0b1a11fd7aa
CptSpooky <legacyofthedonut@gmail.com>
2020-11-21 11:28:31 -0500

    profilesetnetedits

--- client/src/pages/ProfileSetNet/ProfileSetNet.js
+++ client/src/pages/ProfileSetNet/ProfileSetNet.js
@@ -23,9 +23,9 @@ export default function ProfileSetNet() {
                             {/* using short syntax for React fragment with <></> */}
                             <> <h5>GENERAL</h5>
                                 <hr />
-                                <Link className='nav-link' to='/profilesetgen'>
-                                    <h6 style={{ color: 'white' }}>Personal Info</h6>
-                                </Link>
+                            <Link className='nav-link' to='/profilesetgen'>
+                                <h6 style={{ color: 'white' }}>Personal Info</h6>
+                            </Link>
                             </>
                             <><h5 style={{ marginTop: "1em" }}>NETWORKING</h5>
                                 <hr />
