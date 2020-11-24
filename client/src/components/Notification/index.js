3d1f9f62da1d94a98dcf7037e16fbf5cf862f52c
Chelsea Haviland <chelsea.haviland.13@gmail.com>
2020-11-23 23:44:42 -0500

    great success message

--- client/src/components/Notification/index.js
+++ client/src/components/Notification/index.js
@@ -3,23 +3,29 @@ import Toast from 'react-bootstrap/Toast';
 import { Checkmark } from 'react-checkmark';
 
 function Success(props) {
+
     //const [show, setShow] = useState(false);
   
     return (
-     
         //   <Toast onClick={() => setShow(true)} onClose={() => setShow(false)} show={show} delay={3000} autohide>
-            <Toast sytle='zIndex:4' 
+        <Toast style={{
+                zIndex: '4',
+                position: 'absolute',
+                top: 535,
+                right: 30,
+                minHeight: '100px',
+            }} 
                 onClick={props. action} 
                 onClose={props.close} 
                 show={props.show} 
                 delay={props.delay} autohide>
             <Toast.Header>
             <Checkmark size='medium'/>
-              <strong style={{ marginRight: '2em'}}>Success!  </strong>
+              <strong style={{ padding: '.5em', marginRight: '2em'}}>Success!  </strong>
               <small>This message will self destruct.</small>
             </Toast.Header>
             <Toast.Body>Your information was updated successfully.</Toast.Body>
-          </Toast>
+        </Toast>
         
     );
   }
