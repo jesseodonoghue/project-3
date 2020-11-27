c19858cc5df1676b49e72df924f22096a174bcf9
Victoria Greenfield <victoriagfield@gmail.com>
2020-11-27 14:57:23 -0500

    started matching, not working but not breaking anything!

--- client/src/pages/Matching/Matching.js
+++ client/src/pages/Matching/Matching.js
@@ -1,16 +1,80 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 import Form from 'react-bootstrap/Form';
 import { ArrowRightCircle } from 'react-bootstrap-icons';
 import { ArrowLeftCircle } from 'react-bootstrap-icons';
 import './Matching.css';
 import StudentCard from '../../components/StudentCard/StudentCard';
 import MentorCard from '../../components/MentorCard/MentorCard';
+import AUTH from "../../utils/AUTH";
+import API from "../../utils/API";
 
 
 
 
 export default function Matching() {
+    const [user, setUser] = useState({});
+    const [users, setUsers] = useState([]);
+    const [userIndex, setUserIndex] = useState(0);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        loadUser();
+    }, []);
+
+
+    function nextUser(userIndex) {
+        // Ensure that the user index stays within our range of users
+        if (userIndex >= users.length) {
+        userIndex = 0;
+        }
+        setUser(users[userIndex]);
+        setUserIndex(userIndex);
+    }
+
+    function previousUser(userIndex) {
+        // Ensure that the user index stays within our range of users
+        if (userIndex < 0) {
+        userIndex = users.length - 1;
+        }
+        setUser(users[userIndex]);
+        setUserIndex(userIndex);
+    }
+
+    function handleBtnClick(event) {
+        const btnName = event.target.getAttribute("data-value");
+        if (btnName === "next") {
+            const newUserIndex = userIndex + 1;
+            nextUser(newUserIndex);
+        } else {
+            const newUserIndex = userIndex - 1;
+            previousUser(newUserIndex);
+        }
+    }
+
+    function loadUser() {
+        setLoading(true);
+        AUTH.getUser()
+            .then(users => {
+                setUsers(users.data);
+                setUser(users.data[0]);
+                return users.data;
+            })
+            .catch(err => {
+                console.log(err);
+            })
+            .finally(() => {
+                setLoading(false);
+            });
+    };
+
+
     return (
+        <div>
+            {loading && (
+                <p>Loading...</p>
+            )}
+            {!loading && (
+
         <div className="matchingbox">
             <div className="row" id="matchingrow" style={{ width: "100%"}}>
                 <div className="col-md-6" id="studentbox">
@@ -20,18 +84,23 @@ export default function Matching() {
                             <Form.Group controlId="studentSelect">
                                 <Form.Label style={{ color: "white" }}>Filter</Form.Label>
                                 <Form.Control as="select">
+                                    <option>Choose option...</option>
                                     <option>Javascript</option>
-                                    <option>JQuery</option>
-                                    <option>3</option>
-                                    <option>4</option>
-                                    <option>5</option>
+                                    <option>HTML</option>
+                                    <option>CSS</option>
+                                    <option>jQuery</option>
+                                    <option>Node.js</option>
+                                    <option>Express</option>
+                                    <option>React</option>
+                                    <option>MongoDB</option>
+                                    <option>mySQL</option>
                                 </Form.Control>
                             </Form.Group>
                         </Form>
                         <div className="cardandArrowBox">
-                            <ArrowLeftCircle className="arrowsize"/>
+                            <ArrowLeftCircle className="arrowsize" data-value="back" onClick={handleBtnClick} />
                             <StudentCard />
-                            <ArrowRightCircle className="arrowsize" />
+                            <ArrowRightCircle className="arrowsize" data-value="next" onClick={handleBtnClick} />
                         </div>
                     </div> 
                 </div>
@@ -43,22 +112,29 @@ export default function Matching() {
                             <Form.Group controlId="studentSelect">
                                 <Form.Label style={{ color: "#8860D0" }}>Filter</Form.Label>
                                 <Form.Control as="select">
+                                    <option>Choose option...</option>
                                     <option>Javascript</option>
-                                    <option>JQuery</option>
-                                    <option>3</option>
-                                    <option>4</option>
-                                    <option>5</option>
+                                    <option>HTML</option>
+                                    <option>CSS</option>
+                                    <option>jQuery</option>
+                                    <option>Node.js</option>
+                                    <option>Express</option>
+                                    <option>React</option>
+                                    <option>MongoDB</option>
+                                    <option>mySQL</option>
                                 </Form.Control>
                             </Form.Group>
                         </Form>
                         <div className="cardandArrowBox">
-                            <ArrowLeftCircle className="arrowsize" style={{ color: "D8D8D8" }}/>
+                            <ArrowLeftCircle className="arrowsize" style={{ color: "D8D8D8" }} data-value="back" onClick={handleBtnClick} />
                             <MentorCard />
-                            <ArrowRightCircle className="arrowsize" style={{ color: "D8D8D8" }}/>
+                            <ArrowRightCircle className="arrowsize" style={{ color: "D8D8D8" }} data-value="next" onClick={handleBtnClick} />
                         </div>
                     </div> 
                 </div>
             </div>
+        </div>
+            )}
         </div>
     )
 }
