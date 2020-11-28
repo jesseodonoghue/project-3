12b69d1846119cd3d45c2a0415bcb97ed231fb63
Alex Varela <alexvar93@gmail.com>
2020-11-28 15:00:09 -0500

    Post page work in progress

--- server/controllers/postsController.js
+++ server/controllers/postsController.js
@@ -33,24 +33,24 @@ module.exports = {
   },
   findByPostId: function(req, res) {
 
-    // db.Post
-    // .find({ _id: req.params.id })
-    // .then(post => {
-    //   res.json(post);
-    // })
-    // .catch(err => res.status(422).json(err));
-    if (req.user) {
-      db.User
-        .find({ _id: req.user._id })
-        .populate("posts")
-        .then(users => {
-          const userPost = users[0].posts.filter(b => b._id.toString() === req.params.id);
-          res.json({ posts: userPost[0] });
-        })
-        .catch(err => res.status(422).json(err));
-    } else {
-      return res.json({ userPost: null });
-    }
+    db.Post
+    .find({ _id: req.params.id })
+    .then(post => {
+      res.json(post);
+    })
+    .catch(err => res.status(422).json(err));
+    // if (req.user) {
+    //   db.User
+    //     .find({ _id: req.user._id })
+    //     .populate("posts")
+    //     .then(users => {
+    //       const userPost = users[0].posts.filter(b => b._id.toString() === req.params.id);
+    //       res.json({ posts: userPost[0] });
+    //     })
+    //     .catch(err => res.status(422).json(err));
+    // } else {
+    //   return res.json({ userPost: null });
+    // }
   },
   create: function(req, res) {
     db.Post
