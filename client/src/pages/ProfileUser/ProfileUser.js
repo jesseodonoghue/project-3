8978881d8e87dfd05ac6d686480bbbbead5399f5
CptSpooky <legacyofthedonut@gmail.com>
2020-12-08 20:27:35 -0500

    matching page and profile page fixes

--- client/src/pages/ProfileUser/ProfileUser.js
+++ client/src/pages/ProfileUser/ProfileUser.js
@@ -90,9 +90,9 @@ export default function ProfileUser(props) {
             )}
             {!loading && (
                 <div className="box" style={{backgroundSize: "cover", backgroundImage: `url(${bgImg})`, justifyContent:"center", alignItems:"center", overflowX:"auto", height: "100vh"}} onMouseMove={onMove} onScroll={onScroll}>
-                    <div className="row" style={{ display: "flex", width: "100%", marginLeft: "0px", marginRight: "0px", justifyContent:"center", alignItems:"center"}}>
+                    <div className="row" style={{ display: "flex", width: "100%", height: "100%", marginLeft: "0px", marginRight: "0px", justifyContent:"center", alignItems:"center", overflow: "hidden"}}>
                         <div className="col-md-12" id="flexfix" style={{ padding: "0px", justifyContent:"center", alignItems:"center", display: "flex"}}>
-                            <a.div className="profileCard" style={{ width: "100%", maxWidth: "500px", transform: interpBg, boxShadow: "0px 0px 10px black"}}>
+                            <a.div className="profileCard" style={{ width: "100%", maxWidth: "500px", maxHeight: "600px", transform: interpBg, boxShadow: "0px 0px 10px black"}}>
                                 <div className="profileContent" style={{ textAlign: "center" }}>
                                     {profile.image === "" && (
                                         <img src={ProfilePicL} className="profileImg"/>
@@ -100,17 +100,17 @@ export default function ProfileUser(props) {
                                     {profile.image !== "" && (
                                         <img src={profile.image} className="profileImg"/>
                                     )}
-                                    <h3>{profile.firstName} {profile.lastName}</h3>
+                                    <h4 style={{ fontWeight: "bold", color: "#5680E9" }}>{profile.firstName} {profile.lastName}</h4>
                                     <p style={{ wordWrap: "break-word" }}>{profile.bio}</p>
                                     <p style={{ wordWrap: "anywhere" }}><strong>LinkedIn:</strong><br /> <a id="LinkedText" href={profile.linkedin} target="blank">{profile.linkedin}</a></p>
                                     <p style={{ wordWrap: "anywhere" }}><strong>GitHub:</strong><br /> <a id="GitText" href={profile.linkedin} target="blank">{profile.github}</a></p>
 
                                     <hr/>
-                                    <h3 style={{ marginTop: "15px" }}>Skills</h3>
+                                    <h4 style={{ marginTop: "15px", fontWeight: "bold", color: "#5680E9" }}>Skills</h4>
                                     {skillsArr.length ? (
-                                        <ul style={{ textAlign: "left" }}>
+                                        <ul className="profileskills" style={{ textAlign: "left" }}>
                                             {skillsArr.map((skill, i) => (
-                                                <li key={i}>{skill}</li>
+                                                <li style={{marginLeft: "10px"}} key={i}>{skill}</li>
                                             ))}
                                         </ul>
                                     ) : (
